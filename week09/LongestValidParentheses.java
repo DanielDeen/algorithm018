444d5dbd244f24c19bb92ce99a080b1bb8ba7bfb
DanielDeen <ding.xuyong@gmail.com>
2020-12-27 01:57:18 +0800

    week10 homework提交

--- week09/LongestValidParentheses.java
+++ week09/LongestValidParentheses.java
@@ -36,7 +36,7 @@ public class LongestValidParentheses {
                 if (s.charAt(i - 1) == '(') {
                     dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
                 } else if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
-                    dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0);
+                    dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) +2;
                 }
                 longest = Math.max(longest, dp[i]);
             }
